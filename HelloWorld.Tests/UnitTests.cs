1e5103ac86567fd101896ea41c229249ed79fb81
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-12-11 11:15:58 +1000

    NUnit files

---
+++ HelloWorld.Tests/UnitTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+
+namespace HelloWorld.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+			// This code is executed before the unit tests.
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+		
+		[Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+		
+		[Test]
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+    }
+}
